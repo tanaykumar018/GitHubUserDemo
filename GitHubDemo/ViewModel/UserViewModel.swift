3229529e92df51cf52880c9c933b813cdc91934d
tanaykumar018 <tanaykumar018@gmail.com>
2024-07-14 09:39:43 -0500

    - Added Network manager to decouple the network layer.

--- GitHubDemo/ViewModel/UserViewModel.swift
+++ GitHubDemo/ViewModel/UserViewModel.swift
@@ -13,33 +13,22 @@ class UserViewModel: ObservableObject {
     @Published var isLoading = false
     private var cancellables: Set<AnyCancellable> = []
     private var hasLoaded = false
-    let baseURL = "https://api.github.com/users"
-
+    
     func fetchUsers() {
-        guard !hasLoaded else { return }
-        
-        isLoading = true
-        guard let url = URL(string: baseURL) else {
-            isLoading = false
-            return
-        }
-        
-        URLSession.shared.dataTaskPublisher(for: url)
-            .map { $0.data }
-            .decode(type: [User].self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
+        NetworkManager.shared.getData(endpoint: .users, type: User.self)
             .sink(receiveCompletion: { [weak self] completion in
                 self?.isLoading = false
                 switch completion {
                 case .finished:
                     self?.hasLoaded = true
                 case .failure(let error):
-                    print("Error fetching users: \(error)")
+                    print("Error fetching users: \(error.localizedDescription)")
                 }
             }, receiveValue: { [weak self] users in
                 self?.users = users
             })
             .store(in: &cancellables)
     }
+    
 }
 
